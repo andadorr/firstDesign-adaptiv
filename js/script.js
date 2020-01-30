039afe7bdece55633c823ff5ab232bada518c8ae
Andador <slayer.han1997@gmail.com>
2020-01-30 17:56:09 +0300

    adaptiv of example of projects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
 	$('.fullpage').fullpage({
-    scrollOverflow: true,
+		scrollOverflow: true,
+		scrollOvelflowOptions: {click: false},
+		afterRender: function() {
+			$('.projects_last').html($('.projects_slides').length);
+		},
+		afterSlideLoad: function(s, o, d) {
+			$('.projects_first').html(++d.index);
+		}
 	});
 
+	$(document).on('click', '.projects_mini a', changeImg);
 
+	function changeImg (e) {
+		e.preventDefault();
+		$('.projects_big img').attr('src', $(this).attr('data-src'));
+	}
 });
